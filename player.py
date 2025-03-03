b9b72d4c5bb7e7e4fd25af57577d95bea21385b9
mteran13 <mmteran07@gmail.com>
2025-03-03 11:55:15 -0700

    Update player.py

--- player.py
+++ player.py
@@ -1,19 +1,26 @@
 import pygame
 from maze import Maze
+from NPC import NPC
 
-CELL_SIZE = 18
+HARD_SIZE = 6
+MED_SIZE = 18 # default size
+EASY_SIZE = 24
+
 WIDTH = 1188 # total width of screen
 HEIGHT = 486 # total height
-MAZE_WIDTH = WIDTH // 2  // CELL_SIZE 
-MAZE_HEIGHT = HEIGHT // CELL_SIZE
+MAZE_WIDTH = WIDTH // 2  // MED_SIZE 
+MAZE_HEIGHT = HEIGHT // MED_SIZE
+
+DARK_RED = (60, 40, 40)
+DARK_CYAN = (40, 60, 60)
 
 class Player:
     def __init__(self, x, y):
         self.x = x
         self.y = y
     
-    def draw(self, screen):
-        pygame.draw.rect(screen, (255, 0, 0), (self.x * CELL_SIZE, self.y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+    def draw(self, screen, size):
+        pygame.draw.rect(screen, (255, 0, 0), (self.x * size, self.y * size, size, size))
     
     # Player movement
     def movePlayer(playerPos, maze, dx, dy):
@@ -24,9 +31,20 @@ class Player:
                 playerPos[0] += dx
                 playerPos[1] += dy
 
-    # does not work lmao  
-    def score(playerPos, endPoint, score):
-        while playerPos[0] == endPoint[0] and playerPos[1] == endPoint[1]:
+    # Currently broken - not recognizing when players reach endpoint
+    def score(maze, isMaze2, player, score):
+        if player[1] == 25 and player[0] == 31:
             score = score + 1
-            break
-            
+            player[0], player[1] = 1, 1
+            if (isMaze2):
+                offset = MAZE_WIDTH * MED_SIZE
+                color = DARK_CYAN
+                player = NPC(1, 1)
+            else:
+                player = Player(1, 1)
+                offset = 0
+                color = DARK_RED 
+            new = Maze.generateMaze(MAZE_WIDTH, MAZE_HEIGHT)
+            Maze.renderMaze(new, Maze.screen, offset, color, MED_SIZE)   
+              
+
