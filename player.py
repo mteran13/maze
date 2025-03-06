6fce65c3d3cfe5f8c5085a0daea0ba51b2f7656b
mteran13 <mmteran07@gmail.com>
2025-03-06 11:55:22 -0700

    Update player.py

--- player.py
+++ player.py
@@ -11,10 +11,16 @@ HEIGHT = 486 # total height
 MAZE_WIDTH = WIDTH // 2  // MED_SIZE 
 MAZE_HEIGHT = HEIGHT // MED_SIZE
 
+RED = (255, 0, 0)
+CYAN = (0, 255, 255)
 DARK_RED = (60, 40, 40)
 DARK_CYAN = (40, 60, 60)
 
+pScore = 0
+NPCScore = 0
+
 class Player:
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -31,20 +37,31 @@ class Player:
                 playerPos[0] += dx
                 playerPos[1] += dy
 
-    # Currently broken - not recognizing when players reach endpoint
-    def score(maze, isMaze2, player, score):
+    # Works now
+    def score(maze, isMaze2, player):
+        global pScore
+        global NPCScore
+
         if player[1] == 25 and player[0] == 31:
-            score = score + 1
             player[0], player[1] = 1, 1
             if (isMaze2):
                 offset = MAZE_WIDTH * MED_SIZE
                 color = DARK_CYAN
-                player = NPC(1, 1)
+                NPCScore += 1
             else:
-                player = Player(1, 1)
                 offset = 0
-                color = DARK_RED 
-            new = Maze.generateMaze(MAZE_WIDTH, MAZE_HEIGHT)
-            Maze.renderMaze(new, Maze.screen, offset, color, MED_SIZE)   
-              
+                color = DARK_RED
+                pScore += 1
+            nu = Maze.generateMaze(MAZE_WIDTH, MAZE_HEIGHT)
+            Maze.renderMaze(nu, Maze.screen, offset, color, MED_SIZE)
+            return ("player: " + str(pScore) + "NPC: " + str(NPCScore))
+    
+    def drawScore(screen, pScore, NPCScore):
+        pygame.font.init()
+        font = pygame.font.SysFont('ubuntu sans', 15)
+    
+        pScoreText = font.render(f"Player: {pScore}", True, RED)
+        NPCScoreText = font.render(f"NPC: {NPCScore}", True, CYAN)
 
+        screen.blit(pScoreText, (0, 0))
+        screen.blit(NPCScoreText, (WIDTH / 2, 0))
