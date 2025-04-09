c6755bff926530fd2d4120c024895516d0eb0d32
mteran13 <mmteran07@gmail.com>
2025-04-09 11:27:53 -0600

    Update player.py

--- player.py
+++ player.py
@@ -1,12 +1,10 @@
 import pygame
-import random
-from maze import Maze
 from collections import deque
 
 MED_SIZE = 18 # default size
 """
 HARD_SIZE = 6
-EASY_SIZE = 24"
+EASY_SIZE = 24
 implement later if I do difficulty
 """
 
@@ -17,8 +15,8 @@ MAZE_HEIGHT = HEIGHT // MED_SIZE
 
 RED = (255, 0, 0)
 CYAN = (0, 255, 255)
-DARK_RED = (60, 40, 40)
-DARK_CYAN = (40, 60, 60)
+DARK_RED = (70, 0, 0)
+DARK_CYAN = (0, 70, 70)
 
 pScore = 0
 NPCScore = 0
@@ -26,11 +24,12 @@ NPCScore = 0
 directions = ["up", "down", "left", "right"]
 
 class Player:
-
+    #Constructor
     def __init__(self, x, y):
         self.x = x
         self.y = y
     
+    # draws player
     def draw(self, screen, size, offset, color):
         pygame.draw.rect(screen, color, (self.x * size + offset, self.y * size, size, size))
     
@@ -43,34 +42,26 @@ class Player:
                 playerPos[0] += dx
                 playerPos[1] += dy
 
+    # adds to respective scores if player reaches end
     def score(maze, isMaze2, player):
         global pScore
         global NPCScore
-
+        
         if player[1] == 25 and player[0] == 31:
-            player[0], player[1] = 1, 1
             if (isMaze2):
-                offset = MAZE_WIDTH * MED_SIZE
-                color = DARK_CYAN
                 NPCScore += 1
+                player[0], player[1] = 1, 1
             else:
-                offset = 0
-                color = DARK_RED
                 pScore += 1
-            
-            newMaze1 = Maze.generateMaze(MAZE_WIDTH, MAZE_HEIGHT)
-            newMaze2 = Maze.generateMaze(MAZE_WIDTH, MAZE_HEIGHT)
+                player[0], player[1] = 1, 1
 
-            Maze.renderMaze(newMaze1, Maze.screen, 0, color, MED_SIZE)
-            Maze.renderMaze(newMaze2, Maze.screen, offset, color, MED_SIZE)
-            return f"player: {pScore} NPC: {NPCScore}"
-    
+    # draws the score on screen at top    
     def drawScore(screen, pScore, NPCScore):
         pygame.font.init()
         font = pygame.font.SysFont('ubuntu sans', 15)
     
-        pScoreText = font.render(f"Player: {pScore}", True, RED)
-        NPCScoreText = font.render(f"(future) NPC: {NPCScore}", True, CYAN)
+        pScoreText = font.render(f"Player: " + str(pScore), True, RED)
+        NPCScoreText = font.render(f"(future) NPC: " + str(NPCScore), True, CYAN)
 
         screen.blit(pScoreText, (0, 0))
         screen.blit(NPCScoreText, (WIDTH / 2, 0))
@@ -80,6 +71,7 @@ class NPC(Player):
         super().__init__(x, y)
         self.path = []
     
+    # supposed pathfinding - does not work right now
     def findPath(self, maze, start, goal):
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)] # right, down, left, up
         # BFS to find shortest path
@@ -102,7 +94,7 @@ class NPC(Player):
     
     def moveNPC(self, maze, goal):
         # Find next step in path to goal
-        path = self.findPath(maze, [self.x, self.y], goal)
+        self.path = self.findPath(maze, [self.x, self.y], goal)
 
         if self.path:
             nextStep = self.path[0] #Take first step to goal
